31c5ff0d7ebd6f6bbbf278e60382582d5abf7ba7
agholson <87455899+agholson@users.noreply.github.com>
2022-10-30 19:47:53 -0400

    Initial Commit

---
+++ Design Code Demo/Design_Code_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_Code_DemoApp.swift
+//  Design Code Demo
+//
+//  Created by Leone on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Design_Code_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
